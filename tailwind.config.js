7616191f7733780e4df3dba83e0c08e1909f1b8c
Licinia Chirindza <chirindzalicinia8@gmail.com>
2022-06-14 14:53:08 +0200

    primeiro comit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./pages/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
